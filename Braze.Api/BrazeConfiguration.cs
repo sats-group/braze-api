36ed756c8d3b7691bc9e931b340a884bdb7f5b9e
Håvar Nøvik <havar.novik@sats.no>
2026-01-30 15:51:50 +0100

    BrazeProviderFactory.

--- Braze.Api/BrazeConfiguration.cs
+++ Braze.Api/BrazeConfiguration.cs
@@ -1,30 +1,11 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using Braze.Api.UserData;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 
 namespace Braze.Api;
-
-/// <summary>
-/// The options for the Braze API.
-/// </summary>
-public class BrazeOptions
-{
-    /// <summary>
-    /// The base address of the Braze API.
-    /// </summary>
-    [Required]
-    public required Uri BaseAddress { get; init; }
-
-    /// <summary>
-    /// The API key for the Braze API.
-    /// </summary>
-    [Required]
-    public required string ApiKey { get; init; }
-}
 
 /// <summary>
 /// Extension methods for adding Braze.Api services to the DI
@@ -35,17 +16,18 @@ public static class BrazeConfiguration
     /// Add necessary Braze.Api services.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to add the necessary Braze.Api services.</param>
+    /// <param name="key">The key use to register Braze clients as keyed services.</param>
     /// <param name="optionsKey">The key of the config section to bind the <see cref="BrazeOptions"/> to.</param>
     /// <returns>A reference to this instance after the operation has completed.</returns>
-    public static IServiceCollection AddBrazeApi(this IServiceCollection services, string optionsKey = "Braze")
+    public static IServiceCollection AddBrazeApi(this IServiceCollection services, object key, string optionsKey)
     {
         services
             .AddOptions<BrazeOptions>(optionsKey)
-            .Configure<IConfiguration>((obj, config) =>
-                config.GetSection(optionsKey).Bind(obj))
+            .BindConfiguration(optionsKey)
+            .ValidateDataAnnotations()
             .ValidateOnStart();
 
-        services.AddHttpClient<IUserDataClient, UserDataClient>((provider, client) =>
+        services.AddHttpClient(optionsKey, (provider, client) =>
         {
             var optionsMonitor = provider.GetRequiredService<IOptionsMonitor<BrazeOptions>>();
             var options = optionsMonitor.Get(optionsKey);
@@ -54,6 +36,51 @@ public static class BrazeConfiguration
                 new AuthenticationHeaderValue("Bearer", options.ApiKey);
         });
 
+        services.AddKeyedTransient<IUserDataClient, UserDataClient>(
+            key,
+            (provider, _) => new UserDataClient(provider
+                .GetRequiredService<IHttpClientFactory>()
+                .CreateClient(optionsKey)));
+
+        services.AddBrazeProviderFactory();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Add necessary Braze.Api services.
+    /// </summary>
+    /// <remarks>
+    /// If you need to register multiple environments,
+    /// use the overload that also excepts a 'key' so that the clients are registered as keyed services.
+    /// </remarks>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the necessary Braze.Api services.</param>
+    /// <param name="configSection">Optional config section name, default: "Braze".</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    public static IServiceCollection AddBrazeApi(this IServiceCollection services, string configSection = "Braze")
+    {
+        services
+            .AddOptions<BrazeOptions>()
+            .BindConfiguration(configSection)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddHttpClient<IUserDataClient, UserDataClient>((provider, client) =>
+        {
+            var options = provider.GetRequiredService<IOptions<BrazeOptions>>();
+            client.BaseAddress = options.Value.BaseAddress;
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", options.Value.ApiKey);
+        });
+
+        services.AddBrazeProviderFactory();
+
+        return services;
+    }
+
+    private static IServiceCollection AddBrazeProviderFactory(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IBrazeProviderFactory, BrazeProviderFactory>();
         return services;
     }
 }
---
+++ Braze.Api/BrazeProviderFactory.cs
@@ -0,0 +1,62 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Braze.Api.UserData;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Braze.Api;
+
+/// <summary>
+/// The options for the Braze API.
+/// </summary>
+public class BrazeOptions
+{
+    /// <summary>
+    /// The base address of the Braze API.
+    /// </summary>
+    [Required]
+    public required Uri BaseAddress { get; init; }
+
+    /// <summary>
+    /// The API key for the Braze API.
+    /// </summary>
+    [Required]
+    public required string ApiKey { get; init; }
+}
+
+/// <summary>
+/// A provider class that can be used to get different kinds braze clients.
+/// </summary>
+public interface IBrazeProvider
+{
+    /// <summary>
+    /// Gets the <see cref="IUserDataClient"/> from this provider.
+    /// </summary>
+    IUserDataClient UserDataClient { get; }
+}
+
+internal class BrazeProvider(IServiceProvider provider, object? key) : IBrazeProvider
+{
+    private readonly Lazy<IUserDataClient> _userDataClient = new(() =>
+        key is not null
+            ? provider.GetRequiredKeyedService<IUserDataClient>(key)
+            : provider.GetRequiredService<IUserDataClient>());
+
+    public IUserDataClient UserDataClient => _userDataClient.Value;
+}
+
+/// <summary>
+/// A factory class for <see cref="IBrazeProvider"/>.
+/// </summary>
+public interface IBrazeProviderFactory
+{
+    /// <summary>
+    /// Create a <see cref="IBrazeProvider"/>.
+    /// </summary>
+    /// <param name="key">If provided this will create a <see cref="IBrazeProvider"/> that gets Brazed clients as keyed services.</param>
+    /// <returns>A <see cref="IBrazeProvider"/>.</returns>
+    IBrazeProvider Create(object? key = null);
+}
+internal class BrazeProviderFactory(IServiceProvider provider) : IBrazeProviderFactory
+{
+    public IBrazeProvider Create(object? key = null) => new BrazeProvider(provider, key);
+}
