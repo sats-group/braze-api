517c7321bbe6f4b799ecb14ef562f60dddba2cb9
Joachim Løvf <joachimlovf@joachims-mbp.lan>
2026-02-21 17:28:22 +0100

    Moves subscription group state into correct namespace

---
+++ Braze.Api/UserData/SubscriptionGroupSubscribeState.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Braze.Api.UserData;
+
+/// <summary>
+/// A subscription state describing a user's subscription status in a subscription group.
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum SubscriptionGroupSubscribeState
+{
+    /// <summary>
+    /// Not in subscription group
+    /// </summary>
+    [JsonStringEnumMemberName("unsubscribed")]
+    Unsubscribed = 0,
+    /// <summary>
+    /// In subscription group
+    /// </summary>
+    [JsonStringEnumMemberName("subscribed")]
+    Subscribed
+}
